aaa58a848f5e0fcedc33d382777e2b465584bb42
cyberrangeattt-lvtn <cyberrange.attt@gmail.com>
2026-07-29 17:41:51 +0700

    Update image_resizer.py

--- provisioning/roles/webapp/files/image_resizer.py
+++ provisioning/roles/webapp/files/image_resizer.py
@@ -20,7 +20,7 @@ def fetch():
     return out
 @admin.get("/admin")
 def ap():
-    r=Response("ImageResizer administration panel\n"); r.headers["X-Admin-Panel"]="FLAG{5SRF_c0nf1rm3d}"; return r
+    r=Response("ImageResizer administration panel\n"); r.headers["X-Admin-Panel"]="5SRF_c0nf1rm3d"; return r
 if __name__=="__main__":
     threading.Thread(target=lambda: admin.run(host="127.0.0.1",port=8080,use_reloader=False),daemon=True).start()
     app.run(host="0.0.0.0",port=5000,use_reloader=False)
